ceccd61d4c617130272fffe7a0be7fc84231522a
Young <kimyou2019@gmail.com>
2022-07-11 20:34:01 +0200

    db connection errorhandling logger

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,4 +1,8 @@
+const { logEvents } = require('./logEvents');
+
 const errorHandler = (err, req, res, next) => {
+  logEvents(`${err.name}: ${err.message}`, 'errLog.txt');
+  console.error(err.stack);
   const statusCode = res.statusCode ? res.statusCode : 500;
   res.status(statusCode);
   res.json({
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,48 @@
 const express = require('express');
+
+// eslint-disable-next-line no-unused-vars
+const { colors } = require('colors');
+// eslint-disable-next-line no-unused-vars
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+const { logger } = require('./middleware/logEvents');
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+
 const workoutRoutes = require('./routes/workoutsRoutes');
 const userRoutes = require('./routes/userRoutes');
 const postRoutes = require('./routes/postRoutes');
-const dotenv = require('dotenv').config();
-const cors = require('cors');
-const { errorHandler } = require('./middleware/errorMiddleware');
 
 const PORT = process.env.PORT || 7000;
+
+// Connect to database
+connectDB();
 
 // express app
 const app = express();
 
-// middleware
+// custom middleware logger
 app.use(logger);
+
+// Cross Origin Resource Sharing
+const whitelist = [
+  'https://www.google.com',
+  'http://127.0.0.1:8000',
+  'http://localhost:8000',
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
-app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
@@ -32,12 +60,10 @@ app.use('/api/users', userRoutes);
 app.use('/api/workouts', workoutRoutes);
 app.use('/api/posts', workoutRoutes);
 
-function logger(req, res, next) {
-  console.log('------------------');
-  console.log(req.originalUrl);
-  console.log('------------------');
-  next();
-}
+app.all('*', (req, res) => {
+  res.status(404);
+  res.send(`404`);
+});
 
 app.use(errorHandler);
 
